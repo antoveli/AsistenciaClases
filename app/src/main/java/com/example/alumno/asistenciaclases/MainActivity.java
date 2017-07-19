ba0b0e8c7c29c7f0c5b2f1860696c75bdb706e1d
antoveli <antovelisantos@hotmail.com>
2017-07-18 23:36:31 -0600

    segundo commit

--- app/src/main/java/com/example/alumno/asistenciaclases/MainActivity.java
+++ app/src/main/java/com/example/alumno/asistenciaclases/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.alumno.asistenciaclases;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,23 +25,31 @@ public class MainActivity extends AppCompatActivity {
         et1 = (EditText) findViewById(R.id.asignatura1 );
         et2 = (EditText) findViewById(R.id.asignatura2 );
         et3 = (EditText) findViewById(R.id.asignatura3 );
-        chk1 = ()
-
+        chk1 = (CheckBox) findViewById(R.id.chk1);
+        tv1 = (TextView) findViewById(R.id.tv1);
     }
 
-    public void calcular(View view)
+    public void calcular2(View view)
     {
         int numero1 = Integer.parseInt(et1.getText().toString());
         int numero2 = Integer.parseInt(et2.getText().toString());
         int numero3 = Integer.parseInt(et3.getText().toString());
         float promedio;
-        float descuento= 90/100;
+        float descuento= 10/100;
+        float total;
 
         if( chk1.isChecked() )
         {
-            promedio = ((numero1 + numero2 +numero3)/3)-descuento;
+            promedio = ((numero1 + numero2 +numero3)/3);
+            total=promedio*descuento;
 
-            tv1.setText(String.valueOf( promedio ) );
+            tv1.setText(String.valueOf( total ) );
+        }
+        else
+        {
+            promedio = (numero1 + numero2 +numero3)/3;
+            tv1.setText(String.valueOf(promedio));
+
         }
     }
 }
